35ae37ff5a75769b6475f65d0516919bfce0e6d4
Ashistorq <ashishsinghtarwa@gmail.com>
2023-07-28 03:35:49 +0530

    staged changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+import './App.css';
+
+class App extends React.Component {
+  state = {
+    advice: '',
+  }
+
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  
+
+  render() {
+    return (
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{this.state.advice}</h1>
+          <button className="button" onClick={this.fetchAdvice}>
+            <span>GIVE ME ADVICE!</span>
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
